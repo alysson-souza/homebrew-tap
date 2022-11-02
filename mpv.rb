0703ec196dc4d88676b4b534d532213af00a1da4
Alysson Souza <alysson@ll9.com.br>
2022-11-02 17:34:29 +0000

    Initial commit

---
+++ mpv.rb
@@ -0,0 +1,74 @@
+class Mpv < Formula
+  desc "Media player based on MPlayer and mplayer2"
+  homepage "https://mpv.io"
+  url "https://github.com/mpv-player/mpv/archive/v0.34.1.tar.gz"
+  sha256 "32ded8c13b6398310fa27767378193dc1db6d78b006b70dbcbd3123a1445e746"
+  license :cannot_represent
+  revision 2
+  head "https://github.com/mpv-player/mpv.git", branch: "master"
+
+  depends_on "docutils" => :build
+  depends_on "pkg-config" => :build
+  depends_on "python@3.10" => :build
+  depends_on xcode: :build
+  depends_on "ffmpeg"
+  depends_on "jpeg-turbo"
+  depends_on "libarchive"
+  depends_on "libass"
+  depends_on "little-cms2"
+  depends_on "luajit"
+  depends_on "mujs"
+  depends_on "uchardet"
+  depends_on "vapoursynth"
+  depends_on "yt-dlp"
+
+  on_linux do
+    depends_on "alsa-lib"
+  end
+
+  fails_with gcc: "5" # ffmpeg is compiled with GCC
+
+  def install
+    # LANG is unset by default on macOS and causes issues when calling getlocale
+    # or getdefaultlocale in docutils. Force the default c/posix locale since
+    # that's good enough for building the manpage.
+    ENV["LC_ALL"] = "C"
+
+    # Avoid unreliable macOS SDK version detection
+    # See https://github.com/mpv-player/mpv/pull/8939
+    if OS.mac?
+      sdk = (MacOS.version == :big_sur) ? MacOS::Xcode.sdk : MacOS.sdk
+      ENV["MACOS_SDK"] = sdk.path
+      ENV["MACOS_SDK_VERSION"] = "#{sdk.version}.0"
+    end
+
+    # libarchive is keg-only
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["libarchive"].opt_lib/"pkgconfig"
+
+    args = %W[
+      --prefix=#{prefix}
+      --enable-html-build
+      --enable-javascript
+      --enable-libmpv-shared
+      --enable-lua
+      --enable-libarchive
+      --enable-uchardet
+      --confdir=#{etc}/mpv
+      --datadir=#{pkgshare}
+      --mandir=#{man}
+      --docdir=#{doc}
+      --zshdir=#{zsh_completion}
+      --lua=luajit
+    ]
+
+    python3 = "python3.10"
+    system python3, "bootstrap.py"
+    system python3, "waf", "configure", *args
+    system python3, "waf", "install"
+  end
+
+  test do
+    system bin/"mpv", "--ao=null", "--vo=null", test_fixtures("test.wav")
+    assert_match "vapoursynth", shell_output(bin/"mpv --vf=help")
+  end
+end
